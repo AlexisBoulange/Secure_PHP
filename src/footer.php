70f0123a1d7fd02e7b271783d97abb5df443b34a
ZiCkYS <alexisboulange88@gmail.com>
2021-03-10 23:57:08 +0100

    Create footer.php

---
+++ src/footer.php
@@ -0,0 +1,25 @@
+<footer>
+            <div class="container flex">
+                <article>
+                    <h6>ABOUT VIBES</h6>
+                    <div class="smallredline"></div>
+                    <p>VIBES is an ideal theme for creative folks or design agencies. <b>Ease of use and top-notch web
+                            design techniques</b> are wrapped with several gorgeous color schemes.</p>
+                    <p>You can buy this reponsive WordPress on <a href="https://themeforest.net/">ThemeForest</a>.</p>
+                </article>
+                <article>
+                    <h6>NEWSLETTER</h6>
+                    <div class="smallredline"></div>
+                    <p><b>VIBES</b> contains fully working <b>MailChimp</b> subscribe form</p>
+                    <form>
+                        <label for="mail"></label>
+                        <input type="text" id="mail" name="mail" value="Your Email Adress"><br>
+                        <input type="submit" value="SUBSCRIBE">
+                    </form>
+                </article>
+                <article>
+                    <h6>TWITTER FEED</h6>
+                    <div class="smallredline"></div><br>
+                </article>
+            </div>
+        </footer>
