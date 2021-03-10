75d514f3b5b759056d08ac751be7196bb6c8c4a5
ZiCkYS <alexisboulange88@gmail.com>
2021-03-10 23:57:11 +0100

    Create gallery.php

---
+++ src/gallery.php
@@ -0,0 +1,31 @@
+<div class="lightbox">
+			<div class="lightContent flex">
+				<div id="lightLeft"><i class="fas fa-arrow-left"></i></div>
+				<div id="lightImage"></div>
+				<div id="lightRight"><i class="fas fa-arrow-right"></i></div>
+			</div>
+			<div id="lightClose"><i class="fas fa-times"></i></div>
+		</div>
+<section class="gallery">
+    <div class="gallery-items flex">
+        <figure><img src="img/gallery1.jpg" alt="Image retro"></figure>                
+        <figure><img src="img/gallery2.jpg" alt="Image retro2"></figure>
+        <figure><img src="img/gallery3.jpg" alt="Image retro3"></figure>
+        <figure><img src="img/gallery4.jpg" alt="Image retro4"></figure>
+        <figure><img src="img/gallery5.jpg" alt="Image retro5"></figure>
+        <figure><img src="img/gallery6.jpg" alt="Image retro6"></figure>
+        <figure><img src="img/gallery7.jpg" alt="Image retro7"></figure>
+        <figure><img src="img/gallery8.jpg" alt="Image retro8"></figure>
+    </div>
+    <article class="gallerynav flex">
+        <div class="media">
+            <a href="#"><b>ALL</b></a>
+            <a href="#"><b>AUDIO</b></a>
+            <a href="#"><b>IMAGE</b></a>
+            <a href="#"><b>VIDEO</b></a>
+        </div>
+        <div class="loadmore">
+            <a href="#"><b>LOAD MORE</b></a>
+        </div>
+    </article>
+</section>
